9998a8b92b0a3e0435eaf5cdbb4b356976e36cc3
Anton Romanovich <anthony.romanovich@gmail.com>
2013-04-13 01:51:21 +0600

    Use jedi-vim

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-sudo apt-get install autojump terminator
+sudo apt-get install autojump terminator ack
+pip install --user jedi
 pip install --user git+git://github.com/Lokaltog/powerline
 wget https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf -P ~/.fonts
 sudo fc-cache -vf ~/.fonts
