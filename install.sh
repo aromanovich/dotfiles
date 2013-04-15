0f97b5cb501f9df839e2a87b8f0a169a05f9bf62
Anton Romanovich <anthony.romanovich@gmail.com>
2013-04-15 13:24:10 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@ sudo apt-get install autojump terminator ack-grep
 pip install --user jedi
 pip install --user git+git://github.com/Lokaltog/powerline
 
-wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20for%20Powerline.otf -P ~/.fonts
-wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20Italic%20for%20Powerline.otf -P ~/.fonts
-wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20Bold%20for%20Powerline.otf -P ~/.fonts
-wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20Bold%20Italic%20for%20Powerline.otf -P ~/.fonts
+wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20for%20Powerline.otf -N -P ~/.fonts
+wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20Italic%20for%20Powerline.otf -N -P ~/.fonts
+wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20Bold%20for%20Powerline.otf -N -P ~/.fonts
+wget https://github.com/Lokaltog/powerline-fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20Bold%20Italic%20for%20Powerline.otf -N -P ~/.fonts
 
 sudo fc-cache -vf ~/.fonts
+
+git submodule init
+git submodule update
+ln -si ./.config/powerline ~/.config/powerline
+ln -si ./.config/terminator ~/.config/terminator
